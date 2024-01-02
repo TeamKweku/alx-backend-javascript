8e83093c1f147ec36f2ec8be00c8c4160979c758
teamkweku <teamkweku@outlook.com>
2024-01-02 21:57:43 +0000

    checker error fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,24 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      const { body: photoBody } = photoResponse;
-      const { firstName, lastName } = userResponse;
+// export default function handleProfileSignup() {
+//   Promise.all([uploadPhoto(), createUser()])
+//     .then(([photoResponse, userResponse]) => {
+//       const { body: photoBody } = photoResponse;
+//       const { firstName, lastName } = userResponse;
 
-      console.log(`${photoBody} ${firstName} ${lastName}`);
+//       console.log(`${photoBody} ${firstName} ${lastName}`);
+//     })
+//     .catch(() => {
+//       console.error('Signup system offline');
+//     });
+// }
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
