f8c7d6cb12a2e2ba2e7da1a465b973648021541c
teamkweku <teamkweku@outlook.com>
2024-01-04 16:44:22 +0000

    fix for this keyword error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,16 +3,14 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
     }
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
 
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  /* eslint class-methods-use-this: ["error", { "exceptMethods": ["evacuationWarningMessage"] }] */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
