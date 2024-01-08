24e2ccf2981e1e7414f001a3b6ab0421dad95b91
teamkweku <teamkweku@outlook.com>
2024-01-08 12:07:45 +0000

    using the startWidth method on a set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') return '';
+
+  let result = '';
+  for (const item of set) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, -1);
+}
