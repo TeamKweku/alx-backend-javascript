a402f324e578b5a0b5c8ceabe30230210f26c4b4
teamkweku <teamkweku@outlook.com>
2024-01-09 08:35:57 +0000

    checker error fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (
+    !set || !startString || !(set instanceof Set) || typeof startString !== 'string'
+  ) {
+    return '';
+  }
 
   let result = '';
   for (const item of set) {
