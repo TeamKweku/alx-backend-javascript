b0efb7ac95242d6c63285843d866aa02a8adfc3f
teamkweku <teamkweku@outlook.com>
2024-02-29 10:21:15 +0000

    using spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const utils = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(utils.calculateNumber.callCount).to.be.equal(1);
+    utils.calculateNumber.restore();
+  });
+});
